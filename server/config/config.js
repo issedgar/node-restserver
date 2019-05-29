6bc6a99548bd4571addd067540961716edfd735c
edgar.carrillo <issedgar@hotmail.com>
2019-05-28 23:00:12 -0500

    RestServer - usuario

---
+++ server/config/config.js
@@ -0,0 +1,23 @@
+
+
+// =================================
+// PUERTO
+// =================================
+process.env.PORT = process.env.PORT || 3000;
+
+
+// =================================
+// ENTORNO
+// =================================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// =================================
+// BASE DE DATOS
+// =================================
+let urlDB;
+if ( process.env.NODE_ENV === 'dev') {
+  urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+  urlDB = 'mongodb+srv://hcarrillo:oYHG31Epx7gwxL7o@cluster0-hyzq7.mongodb.net/cafe';
+}
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 const color = require('colors/safe');
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 
-require('../config/config');
+require('./config/config');
 
 const bodyParser = require('body-parser');
-
-const port = process.env.PORT || 3000;
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -14,45 +14,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
+app.use( require('./routes/usuario'))
 
-app.get('/', (req, res) => {  
-  res.json('Hola mundo cruel');
-});
 
-app.get('/usuario', (req, res) => {  
-  res.json('get usuario');
-});
+mongoose.connect(process.env.URLDB, { useCreateIndex: true, useNewUrlParser: true, useFindAndModify: false }, (err, res) => {
+  if( err ) throw err;
 
-app.post('/usuario', (req, res) => {  
-
-  let body = req.body;
-
-  if ( body.nombre === undefined ) {
-
-    res.status(400).json({
-      ok: false,
-      mensaje: 'El nombre es necesario'
-    });
-
-  } else {
-    res.json({
-      persona: body
-    });
-  }
-
-});
-
-app.put('/usuario/:id', (req, res) => {  
-
-  let id = req.params.id;
-
-  res.json({
-    id,
-    data: 'put usuario'});
-});
-
-app.delete('/usuario', (req, res) => {  
-  res.json('delete usuario');
+  console.log('Base de datos mongoDB', color.green('onLine'));
 });
 
 app.listen(process.env.PORT, () => {
